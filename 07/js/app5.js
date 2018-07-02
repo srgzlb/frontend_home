5ac05aba9765befc6c1166c40c94a3f6e0f0a433
ArtiomUshkov <fr1m0900@gmail.com>
2018-07-02 20:09:45 +0300

    commit1

--- 07/js/app5.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let x = prompt('какого ты года рождения?');
-const year = 2018;
-let age = year - x;
-if (age > 16) alert("добро пожаловать");
-if (age < 16) alert("вход воспрещен");
